05fe93321d1da95c93e8ff42d389b07c82853f02
Sarabeth <s.moody1985@gmail.com>
2021-03-12 20:05:55 -0500

    All but weakest

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -5,172 +5,276 @@
 #include "extrafile.h"
 using namespace std;
 
-//void ReadFromFile(string inFile, vector<Ship>& ships) {
-//	ifstream file(inFile, ios::binary);
-//
-//	//Read number of ships 
-//	unsigned int count = 0;
-//	file.read((char*)&count, sizeof(count));
-//	cout << count << endl;
-//
-//	for (unsigned int i = 0; i < count; i++) {
-//		int length;
-//
-//		//Read the length of the string
-//		file.read((char*)&length, sizeof(length));
-//
-//		//Dynamicallly allocate an array of chars of that size
-//		char* buffer = new char[length];
-//
-//		//Read into that dyname array
-//		file.read(buffer, length);
-//
-//		//Copy dynamic array into a std::string
-//		string shipName = buffer;
-//
-//		//Delete dynamic array
-//		delete[] buffer;
-//		//cout << shipName << endl;
-//
-//		//Read the class
-//		file.read((char*)&length, sizeof(length));
-//		buffer = new char[length];
-//		file.read(buffer, length);
-//		string shipClass = buffer;
-//		delete[] buffer;
-//		//cout << shipClass << endl;
-//
-//		//Read the ship length
-//		short shipLength;
-//		file.read((char*)&shipLength, sizeof(shipLength));
-//		//cout << shipLength << endl;
-//
-//		//Read the shield capacity
-//		int shieldCapacity;
-//		file.read((char*)&shieldCapacity, sizeof(shieldCapacity));
-//		//cout << shieldCapacity << endl;
-//
-//		//Read the warp speed
-//		float warpSpeed;
-//		file.read((char*)&warpSpeed, sizeof(warpSpeed));
-//		//cout << warpSpeed << endl;
-//
-//		//Read the number of weapons
-//		int numOfWeapons;
-//		file.read((char*)&numOfWeapons, sizeof(numOfWeapons));
-//		//cout << numOfWeapons << endl;
-//
-//		Ship ship(shipName, shipClass, shipLength, shieldCapacity, warpSpeed);
-//		ships.push_back(ship);
-//	}
-//}
-
-int main()
-{	
-	vector<Ship> ships;
-	//vector<Weapon> weapons;
-	//cout << "Which file(s) to open?\n";
-	//cout << "1. friendlyships.shp" << endl;
-	//cout << "2. enemyships.shp" << endl;
-	//cout << "3. Both files" << endl;
-	//int option;
-	//cin >> option;
-
-	////Load files here 
-	//if (option == 1) {
-	//	ReadFromFile("enemyships.shp", ships, weapons);
-	//}
-
-	//cout << "1. Print all ships" << endl;
-	//cout << "2. Starship with the strongest weapon" << endl;
-	//cout << "3. Strongest starship overall" << endl;
-	//cout << "4. Weakest ship (ignoring unarmed)" << endl;
-	//cout << "5. Unarmed ships" << endl;
-
-	//cin >> option;
-
-	/* Work your magic here */
-
-	ifstream file("enemyships.shp", ios::binary);
+void ReadFromFile(string inFile, vector<Ship>& ships) {
+	ifstream file(inFile, ios::binary);
 
 	//Read number of ships 
 	unsigned int count = 0;
 	file.read((char*)&count, sizeof(count));
-	cout << count << endl;
+	//cout << count << endl;
 
-	int length;
+	for (unsigned int i = 0; i < count; i++) {
+		int length;
 
-	//Read the length of the string
-	file.read((char*)&length, sizeof(length));
+		//Read the length of the string
+		file.read((char*)&length, sizeof(length));
 
-	//Dynamicallly allocate an array of chars of that size
-	char* buffer = new char[length];
+		//Dynamicallly allocate an array of chars of that size
+		char* buffer = new char[length];
 
-	//Read into that dyname array
-	file.read(buffer, length);
+		//Read into that dyname array
+		file.read(buffer, length);
 
-	//Copy dynamic array into a std::string
-	string shipName = buffer;
+		//Copy dynamic array into a std::string
+		string shipName = buffer;
 
-	//Delete dynamic array
-	delete[] buffer;
-	cout << shipName << endl;
+		//Delete dynamic array
+		delete[] buffer;
+		//cout << shipName << endl;
 
-	//Read the class
-	file.read((char*)&length, sizeof(length));
-	buffer = new char[length];
-	file.read(buffer, length);
-	string shipClass = buffer;
-	delete[] buffer;
-	cout << shipClass << endl;
+		//Read the class
+		file.read((char*)&length, sizeof(length));
+		buffer = new char[length];
+		file.read(buffer, length);
+		string shipClass = buffer;
+		delete[] buffer;
+		//cout << shipClass << endl;
 
-	//Read the ship length
-	short shipLength;
-	file.read((char*)&shipLength, sizeof(shipLength));
-	cout << shipLength << endl;
+		//Read the ship length
+		short shipLength;
+		file.read((char*)&shipLength, sizeof(shipLength));
+		//cout << shipLength << endl;
 
-	//Read the shield capacity
-	int shieldCapacity;
-	file.read((char*)&shieldCapacity, sizeof(shieldCapacity));
-	cout << shieldCapacity << endl;
+		//Read the shield capacity
+		int shieldCapacity;
+		file.read((char*)&shieldCapacity, sizeof(shieldCapacity));
+		//cout << shieldCapacity << endl;
 
-	//Read the warp speed
-	float warpSpeed;
-	file.read((char*)&warpSpeed, sizeof(warpSpeed));
-	cout << warpSpeed << endl;
+		//Read the warp speed
+		float warpSpeed;
+		file.read((char*)&warpSpeed, sizeof(warpSpeed));
+		//cout << warpSpeed << endl;
 
-	//Read the number of weapons
-	int numOfWeapons;
-	file.read((char*)&numOfWeapons, sizeof(numOfWeapons));
-	cout << numOfWeapons << endl;
+		//Read the number of weapons
+		int numOfWeapons;
+		file.read((char*)&numOfWeapons, sizeof(numOfWeapons));
+		//cout << numOfWeapons << endl;
 
-	//Read the weapon name
-	file.read((char*)&length, sizeof(length));
-	buffer = new char[length];
-	file.read(buffer, length);
-	string weaponName = buffer;
-	delete[] buffer;
-	cout << weaponName << endl;
+		vector<Ship::Weapon> weapons;
+		for (int j = 0; j < numOfWeapons; j++) {
+			file.read((char*)&length, sizeof(length));
+			buffer = new char[length];
+			file.read(buffer, length);
+			string weaponName = buffer;
+			delete[] buffer;
 
-	//Read the power rating
-	int powerRating;
-	file.read((char*)&powerRating, sizeof(powerRating));
-	cout << powerRating << endl;
+			int powerRating;
+			file.read((char*)&powerRating, sizeof(powerRating));
 
-	//Read the power
-	float power;
-	file.read((char*)&power, sizeof(power));
-	cout << power << endl;
+			float power;
+			file.read((char*)&power, sizeof(power));
+			weapons.push_back(Ship::Weapon(weaponName, powerRating, power));
+		}
 
-	
-	/*for (unsigned int i = 0; i < 2; i++) {
-		cout << ships[i].GetName() << endl;
-		cout << ships[i].GetShipClass() << endl;
-		cout << ships[i].GetShipLength() << endl;
-		cout << ships[i].GetShieldCapacity() << endl;
-		cout << ships[i].GetWarpSpeed() << endl;
+		ships.push_back(Ship(shipName, shipClass, shipLength, shieldCapacity, warpSpeed, weapons));
+	}
+}
+
+void PrintShips(vector<Ship>& ships) {
+	unsigned int sum = 0;
+	for (unsigned int i = 0; i < ships.size(); i++) {
+		cout << "Name: " << ships[i].GetName() << endl;
+		cout << "Class: " << ships[i].GetShipClass() << endl;
+		cout << "Length: " << ships[i].GetShipLength() << endl;
+		cout << "Shield capacity: " << ships[i].GetShieldCapacity() << endl;
+		cout << "Maximum Warp: " << ships[i].GetWarpSpeed() << endl;
+		cout << "Armaments: " << endl;
+		unsigned int count = ships[i].weapons.size();
+		if (count == 0) {
+			cout << "Unarmed" << endl;
+		}
+		else {
+			for (unsigned int j = 0; j < ships[i].weapons.size(); j++) {
+				cout << ships[i].weapons[j].GetWeaponName() << ", " << ships[i].weapons[j].GetPowerRating() << ", " << ships[i].weapons[j].GetPower() << endl;
+				sum += ships[i].weapons[j].GetPowerRating();
+			}
+			cout << "Total firepower: " << sum << endl;
+			sum = 0;
+		}
+		cout << " " << endl;
+	}
+}
+
+void StrongestWeapon(vector<Ship>& ships) {
+	unsigned int max = 0;
+	unsigned int sum = 0;
+	for (unsigned int i = 0; i < ships.size(); i++) {
+		for (unsigned int j = 0; j < ships[i].weapons.size(); j++) {
+			if (ships[i].weapons[j].GetPowerRating() > max) {
+				max = ships[i].weapons[j].GetPowerRating();
+			}
+		}
+		break;
+	}
+	int index = 0;
+	for (unsigned int i = 0; i < ships.size(); i++) {
+		for (unsigned int j = 0; j < ships[i].weapons.size(); j++) {
+			if (ships[i].weapons[j].GetPowerRating() == max) {
+				index = i;
+			}
+		}
+	}
+	cout << "Name: " << ships[index].GetName() << endl;
+	cout << "Class: " << ships[index].GetShipClass() << endl;
+	cout << "Length: " << ships[index].GetShipLength() << endl;
+	cout << "Shield capacity: " << ships[index].GetShieldCapacity() << endl;
+	cout << "Maximum Warp: " << ships[index].GetWarpSpeed() << endl;
+	cout << "Armaments: " << endl;
+	for (unsigned int j = 0; j < ships[index].weapons.size(); j++) {
+		cout << ships[index].weapons[j].GetWeaponName() << ", " << ships[index].weapons[j].GetPowerRating() << ", " << ships[index].weapons[j].GetPower() << endl;
+		sum += ships[index].weapons[j].GetPowerRating();
+	}
+	cout << "Total firepower: " << sum << endl;
+	sum = 0;
+}
+
+void StrongestOverall(vector<Ship>& ships) {
+	vector<unsigned int> sums;
+	unsigned int sum = 0;
+	unsigned int max = 0;
+	for (unsigned int i = 0; i < ships.size(); i++) {
+		for (unsigned int j = 0; j < ships[i].weapons.size(); j++) {
+			sum += ships[i].weapons[j].GetPowerRating();
+		}
+		sums.push_back(sum);
+		sum = 0;
+	}
+	int index = 0;
+	for (unsigned int k = 0; k < sums.size(); k++) {
+		if (sums[k] > max) {
+			max = sums[k];
+			index = k;
+		}
+	}
+	cout << "Name: " << ships[index].GetName() << endl;
+	cout << "Class: " << ships[index].GetShipClass() << endl;
+	cout << "Length: " << ships[index].GetShipLength() << endl;
+	cout << "Shield capacity: " << ships[index].GetShieldCapacity() << endl;
+	cout << "Maximum Warp: " << ships[index].GetWarpSpeed() << endl;
+	cout << "Armaments: " << endl;
+	for (unsigned int j = 0; j < ships[index].weapons.size(); j++) {
+		cout << ships[index].weapons[j].GetWeaponName() << ", " << ships[index].weapons[j].GetPowerRating() << ", " << ships[index].weapons[j].GetPower() << endl;
+		sum += ships[index].weapons[j].GetPowerRating();
+	}
+	cout << "Total firepower: " << sum << endl;
+	sum = 0;
+}
+
+void WeakestOverall(vector<Ship>& ships) {
+	vector<unsigned int> sums;
+	unsigned int sum = 0;
+	for (unsigned int i = 0; i < ships.size(); i++) {
+		unsigned int count = ships[i].weapons.size();
+		if (count > 0) {
+			for (unsigned int j = 0; j < ships[i].weapons.size(); j++) {
+				sum += ships[i].weapons[j].GetPowerRating();
+			}
+		}
+		sums.push_back(sum);
+		sum = 0;
+	}
+	/*for (int k = 0; k < sums.size(); k++) {
+		cout << sums[k] << " ";
 	}*/
+	int index = 0;
+	int min;
+	for (unsigned int k = 0; k < sums.size(); k++) {
+		if (sums[k] != 0) {
+			min = sums[k];
+		}
+		if (sums[k] < min) {
+			min = sums[k];
+			cout << min << endl;
+		}
+	}
+	/*cout << "Name: " << ships[index].GetName() << endl;
+	cout << "Class: " << ships[index].GetShipClass() << endl;
+	cout << "Length: " << ships[index].GetShipLength() << endl;
+	cout << "Shield capacity: " << ships[index].GetShieldCapacity() << endl;
+	cout << "Maximum Warp: " << ships[index].GetWarpSpeed() << endl;
+	cout << "Armaments: " << endl;
+	for (unsigned int j = 0; j < ships[index].weapons.size(); j++) {
+		cout << ships[index].weapons[j].GetWeaponName() << ", " << ships[index].weapons[j].GetPowerRating() << ", " << ships[index].weapons[j].GetPower() << endl;
+		sum += ships[index].weapons[j].GetPowerRating();
+	}
+	cout << "Total firepower: " << sum << endl;
+	sum = 0;*/
+}
 
+void PrintUnarmedShips(vector<Ship>& ships) {
+	for (unsigned int i = 0; i < ships.size(); i++) {
+		unsigned int count = ships[i].weapons.size();
+		if (count == 0) {
+			cout << "Name: " << ships[i].GetName() << endl;
+			cout << "Class: " << ships[i].GetShipClass() << endl;
+			cout << "Length: " << ships[i].GetShipLength() << endl;
+			cout << "Shield capacity: " << ships[i].GetShieldCapacity() << endl;
+			cout << "Maximum Warp: " << ships[i].GetWarpSpeed() << endl;
+			cout << "Armaments: " << endl;
+			cout << "Unarmed" << endl;
+		}
+	}
+}
 
+int main()
+{	
+	vector<Ship> ships;
+	cout << "Which file(s) to open?\n";
+	cout << "1. friendlyships.shp" << endl;
+	cout << "2. enemyships.shp" << endl;
+	cout << "3. Both files" << endl;
+	int option;
+	cin >> option;
+
+	//Load files here 
+
+	cout << " " << endl;
+	if (option == 1) {
+		ReadFromFile("friendlyships.shp", ships);
+	}
+	else if (option == 2) {
+		ReadFromFile("enemyships.shp", ships);
+	}
+	else if (option == 3) {
+		ReadFromFile("friendlyships.shp", ships);
+		ReadFromFile("enemyships.shp", ships);
+	}
+
+	cout << "1. Print all ships" << endl;
+	cout << "2. Starship with the strongest weapon" << endl;
+	cout << "3. Strongest starship overall" << endl;
+	cout << "4. Weakest ship (ignoring unarmed)" << endl;
+	cout << "5. Unarmed ships" << endl;
+	cin >> option;
+
+	/* Work your magic here */
+
+	cout << " " << endl;
+	if (option == 1) {
+		PrintShips(ships);
+	}
+	else if (option == 2) {
+		StrongestWeapon(ships);
+	}
+	else if (option == 3) {
+		StrongestOverall(ships);
+	}
+	else if (option == 4) {
+		WeakestOverall(ships);
+	}
+	else if (option == 5) {
+		PrintUnarmedShips(ships);
+	}
+	
 	return 0;
 }
--- Lab5/extrafile.h
+++ Lab5/extrafile.h
@@ -4,59 +4,27 @@
 #include <vector>
 using namespace std;
 
-//class Weapon {
-//public:
-//	Weapon(string weaponName, unsigned int powerRating, float power);
-//	string GetWeaponName();
-//	unsigned int GetPowerRating();
-//	float GetPower();
-//
-//private: 
-//	string weaponName;
-//	unsigned int powerRating;
-//	float power;
-//};
-//
-//Weapon::Weapon(string weaponName, unsigned int powerRating, float power) {
-//	this->weaponName = weaponName;
-//	this->powerRating = powerRating;
-//	this->power = power;
-//}
-//
-//string Weapon::GetWeaponName() {
-//	return weaponName;
-//}
-//
-//unsigned int Weapon::GetPowerRating() {
-//	return powerRating;
-//}
-//
-//float Weapon::GetPower() {
-//	return power;
-//}
-
 class Ship {
 public:
-	/*class Weapon {
+	class Weapon {
+	public:
 		string weaponName;
 		unsigned int powerRating;
 		float power;
 
+		Weapon(string weaponName, unsigned int powerRating, float power);
 		string GetWeaponName();
 		unsigned int GetPowerRating();
 		float GetPower();
-	};*/
+	};
 
-	Ship(string shipName, string shipClass, unsigned short shipLength, unsigned int shieldCapacity, float warpSpeed, string weaponName, unsigned int powerRating, float power);
+	Ship(string shipName, string shipClass, unsigned short shipLength, unsigned int shieldCapacity, float warpSpeed, vector<Weapon> weapons);
 	string GetName();
 	string GetShipClass();
 	unsigned short GetShipLength();
 	unsigned int GetShieldCapacity();
 	float GetWarpSpeed();
-	string GetWeaponName();
-	unsigned int GetPowerRating();
-	float GetPower();
-	//unsigned int GetNumOfWeapons();
+	vector<Weapon> weapons;
 
 private:
 	string shipName;
@@ -64,22 +32,15 @@ private:
 	unsigned short shipLength;
 	unsigned int shieldCapacity;
 	float warpSpeed;
-	string weaponName;
-	unsigned int powerRating;
-	float power;
-	//unsigned int numOfWeapons;
 };
 
-Ship::Ship(string shipName, string shipClass, unsigned short shipLength, unsigned int shieldCapacity, float warpSpeed, string weaponName, unsigned int powerRating, float power) {
+Ship::Ship(string shipName, string shipClass, unsigned short shipLength, unsigned int shieldCapacity, float warpSpeed, vector<Weapon> weapons) {
 	this->shipName = shipName;
 	this->shipClass = shipClass;
 	this->shipLength = shipLength;
 	this->shieldCapacity = shieldCapacity;
 	this->warpSpeed = warpSpeed;
-	this->weaponName = weaponName;
-	this->powerRating = powerRating;
-	this->power = power;
-	//this->numOfWeapons = numOfWeapons;
+	this->weapons = weapons;
 }
 
 string Ship::GetName() {
@@ -102,19 +63,21 @@ float Ship::GetWarpSpeed() {
 	return warpSpeed;
 }
 
-//unsigned int Ship::GetNumOfWeapons() {
-//	return numOfWeapons;
-//}
+Ship::Weapon::Weapon(string weaponName, unsigned int powerRating, float power){
+	this->weaponName = weaponName;
+	this->powerRating = powerRating;
+	this->power = power;
+}
 
-string Ship::GetWeaponName() {
+string Ship::Weapon::GetWeaponName() {
 	return weaponName;
 }
 
-unsigned int Ship::GetPowerRating() {
+unsigned int Ship::Weapon::GetPowerRating() {
 	return powerRating;
 }
 
-float Ship::GetPower() {
+float Ship::Weapon::GetPower() {
 	return power;
 }
 
